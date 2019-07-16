ea0ed20e89e8ee143ddb935d2cf3b3dd47e6ded0
deepak180611 <52686653+deepak180611@users.noreply.github.com>
2019-07-15 22:00:54 -0700

    Add files via upload
    homepage

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap-4.3.1-dist\css\bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-4.3.1-dist\js\bootstrap.min.js">
+    <title>home</title>
+</head>
+<body>
+   <div class="jumbotron">
+     <h1>  Welcome My friend</h1>
+     <a href="signup.php" class="btn btn-primary">sign up</a>
+     <a href="login.php" class="btn btn-primary">Log in</a>
+   </div>
+</body>
+</html>
